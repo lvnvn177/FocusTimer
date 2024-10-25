f4f9d0ab7356cc64457b4ed8e640c852715319ba
lvnvn177 <53424393+lvnvn177@users.noreply.github.com>
2024-10-26 00:44:48 +0900

    version_1.0.2

--- FocusTimer/View/Tab/Setting/SoundSelectionView.swift
+++ FocusTimer/View/Tab/Setting/SoundSelectionView.swift
@@ -15,49 +15,70 @@ struct SoundSelectionView: View {
         Form {
             Section {
                 ForEach(settingViewModel.alarmOptions, id: \.self) { sound in
-                    HStack {
-                        Text(sound.rawValue.replacingOccurrences(of: ".mp3", with: ""))
-                        Spacer()
+                    ZStack {
                         if settingViewModel.set.Alarm == sound.rawValue {
-                            Image(systemName: "checkmark")
-                                .foregroundStyle(.blue)
+                            Color("Setting").opacity(0.9) // Color for selected item
+                       } else {
+                           Color.clear // Default color for non-selected items
+                       }
+                        HStack {
+                            Text(sound.rawValue.replacingOccurrences(of: ".mp3", with: ""))
+                                .foregroundStyle(
+                                    settingViewModel.set.Alarm == sound.rawValue ? .white : .primary
+                                )
+                               
+                        }
+                        .clipShape(.rect(cornerRadius: 10))
+                        .contentShape(Rectangle())
+                        .onTapGesture {
+                            print("sound_check:", sound.rawValue)
+                            settingViewModel.set = Setting(
+                                Alarm: sound.rawValue,
+                                Back: settingViewModel.set.Back
+                            )
+    //                        print("SettingView에서 선택한 알림음: \(settingViewModel.setting.append())!!!!")
                         }
                     }
-                    .contentShape(Rectangle())
-                    .onTapGesture {
-                        print("sound_check:", sound.rawValue)
-                        settingViewModel.set = Setting(Alarm: sound.rawValue, Back: settingViewModel.set.Back)
-//                        print("SettingView에서 선택한 알림음: \(settingViewModel.setting.append())!!!!")
-                    }
+            
                 }
             } header: {
-                Text("알림음 선택")
+                Text("알림음")
                     .font(.headline)
                     .fontWeight(.bold)
             }
             
             Section {
                 ForEach(settingViewModel.SoundOptions, id: \.self) { sound in
-                    HStack {
-                        Text(sound.rawValue)
-                        Spacer()
+                    ZStack {
                         if settingViewModel.set.Back == sound.rawValue {
-                            Image(systemName: "checkmark")
-                                .foregroundStyle(.blue)
+                            Color("Setting").opacity(0.9) // Color for selected item
+                       } else {
+                           Color.clear // Default color for non-selected items
+                       }
+                        HStack {
+                            Text(sound.rawValue)
+                                .foregroundStyle(
+                                    settingViewModel.set.Back == sound.rawValue ? .white : .primary
+                                )
+                        }
+                        .contentShape(Rectangle())
+                        .onTapGesture {
+                            print("sound_back_check:", sound.rawValue)
+                            settingViewModel.set = Setting(
+                                Alarm: settingViewModel.set.Alarm,
+                                Back: sound.rawValue
+                            )
                         }
                     }
-                    .contentShape(Rectangle())
-                    .onTapGesture {
-                        print("sound_back_check:", sound.rawValue)
-                        settingViewModel.set = Setting(Alarm: settingViewModel.set.Alarm, Back: sound.rawValue)
-                    }
+                  
                 }
             } header: {
-                 Text("배경음 선택")
+                 Text("배경음")
                     .font(.headline)
                     .fontWeight(.bold)
             }
         }
+        
         .navigationTitle("소리 설정")
     }
 }
--- FocusTimer/ViewModel/SettingViewModel.swift
+++ FocusTimer/ViewModel/SettingViewModel.swift
@@ -30,25 +30,27 @@ class SettingViewModel: ObservableObject {
     }
     
     let alarmOptions: [AlarmSound] = [
-        .sound1, .sound2, .defaultSound, .none
+        .sound1, .sound2, .sound3, .sound4, .none
     ]
     
     let SoundOptions: [BackgroundSound] = [
-        .b_sound1, .none
+        .b_sound1, .b_sound2,.none
     ]
     
     
     enum AlarmSound: String, CaseIterable {
-        case sound1 = "Water.mp3"
-        case sound2 = "Pages.mp3"
-        case defaultSound = ".defaultSound"
-        case none = "None.mp3"
+        case sound1 = "종.mp3"
+        case sound2 = "페이지 넘김.mp3"
+        case sound3 = "물.mp3"
+        case sound4 = "커피 만들기.mp3"
+//        case defaultSound = ".defaultSound"
+        case none = "없음.mp3"
     }
     
     enum BackgroundSound: String, CaseIterable {
-        case b_sound1 = "OceanWave"
-//        case b_sound2 = "Campfire"
-        case none = "None"
+        case b_sound1 = "해안가"
+        case b_sound2 = "캠프파이어"
+        case none = "없음"
     }
 }
 
